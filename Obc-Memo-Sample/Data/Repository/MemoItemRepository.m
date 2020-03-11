e5fc061f86ec544f6eebacf3448db50d26572b9a
Dai Kawahara <skbsporta7@gmail.com>
2020-03-12 00:17:48 +0900

    add MemoItemRepositoryImpl

--- Obc-Memo-Sample/Data/Repository/MemoItemRepository.m
+++ Obc-Memo-Sample/Data/Repository/MemoItemRepository.m
@@ -6,8 +6,10 @@
 //  Copyright © 2020 kawaharadai. All rights reserved.
 //
 
+#import <CoreData/CoreData.h>
 #import "MemoItemRepository.h"
 #import "MemoItemDataStore.h"
+#import "CoreDataError.h"
 
 @interface MemoItemRepositoryImpl ()
 @property (nonatomic) NSObject<MemoItemDataStore> * _Nonnull memoItemDataStore;
@@ -35,32 +37,109 @@
 }
 
 - (void)createMemoItem:(NSString * _Nonnull)text uniqueId:(NSString * _Nullable)uniqueId completion:(void (^ _Nonnull)(MemoItem * _Nullable, NSInteger * _Nullable))completion {
-    <#code#>
+    [self.memoItemDataStore create:@"MemoItem" completion:^(MemoItem * _Nullable memoItem, CoreDataError * _Nullable error) {
+        if (error != nil) {
+            completion(nil, error);
+        } else {
+            NSManagedObjectContext * _Nullable context = memoItem.managedObjectContext;
+            if (context == nil) {
+                completion(nil, NotFoundContext);
+            } else {
+                [context performBlockAndWait:^{
+                    // メモ内容更新
+
+                    [self.memoItemDataStore save:memoItem];
+                }];
+                completion(memoItem, nil);
+            }
+        }
+    }];
 }
 
 
-- (void)deleteAllMemoItems:(NSString * _Nonnull)entityName completion:(void (^ _Nonnull)(void * _Nullable (*)(void), NSInteger * _Nullable))completion {
-    <#code#>
+- (void)deleteAllMemoItems:(NSString * _Nonnull)entityName
+                completion:(void (^ _Nonnull)(void * _Nullable (*)(void), NSInteger * _Nullable))completion {
+    NSFetchRequest * request = [[NSFetchRequest alloc] initWithEntityName:entityName];
+    NSBatchDeleteRequest * deleteRequest = [[NSBatchDeleteRequest alloc] initWithFetchRequest:request];
+    CoreDataError * error = [self.memoItemDataStore execute:deleteRequest];
+    if (error == nil) {
+        completion(nil, nil);
+    } else {
+        completion(nil, error);
+    }
 }
 
 
-- (void)deleteMemoItem:(NSString * _Nonnull)uniqueId completion:(void (^ _Nonnull)(void * _Nullable (*)(void), NSInteger * _Nullable))completion {
-    <#code#>
+- (void)deleteMemoItem:(NSString * _Nonnull)uniqueId
+            completion:(void (^ _Nonnull)(void * _Nullable (*)(void), NSInteger * _Nullable))completion {
+    [self readMemoItem:uniqueId completion:^(MemoItem * _Nullable memoItem, NSInteger * _Nullable errorCode) {
+        if (errorCode != nil) {
+            completion(nil, errorCode);
+        } else {
+            [self.memoItemDataStore delete:memoItem completion:^{
+                completion(nil, nil);
+            }];
+        }
+    }];
 }
 
 
 - (void)readAllMemoItems:(void (^ _Nonnull)(NSArray<MemoItem *> * _Nullable, NSInteger * _Nullable))completion {
-    <#code#>
+    bool ascending = NO;
+    [self.memoItemDataStore fetchArray:[NSCompoundPredicate new]
+                               sortKey:@"editDate"
+                             ascending:&ascending
+                            completion:^(NSArray<MemoItem *> * _Nonnull memoItems, CoreDataError * _Nullable error) {
+        if (error != nil) {
+            completion([NSArray<MemoItem *> new], error);
+        } else {
+            completion(memoItems, nil);
+        }
+    }];
 }
 
 
-- (void)readMemoItem:(NSString * _Nonnull)uniqueId completion:(void (^ _Nonnull)(MemoItem * _Nullable, NSInteger * _Nullable))completion {
-    <#code#>
+- (void)readMemoItem:(NSString * _Nonnull)uniqueId
+          completion:(void (^ _Nonnull)(MemoItem * _Nullable, NSInteger * _Nullable))completion {
+    bool ascending = NO;
+    NSPredicate * predicate = [NSPredicate predicateWithFormat:@"uniqueId == @%", uniqueId];
+    [self.memoItemDataStore fetchArray:[[NSCompoundPredicate alloc] initWithType:NSAndPredicateType
+                                                                   subpredicates:[[NSArray<NSPredicate *> alloc] initWithObjects:predicate, nil]]
+                               sortKey:@"editDate"
+                             ascending:&ascending
+                            completion:^(NSArray<MemoItem *> * _Nonnull memoItems, CoreDataError * _Nullable error) {
+        if (error != nil) {
+            completion(nil, error);
+        } else {
+            if (memoItems.count > 0) {
+                completion(memoItems[0], nil);
+            } else {
+                completion(nil, FailedFetchMemoById);
+            }
+        }
+    }];
 }
 
 
-- (void)updateMemoItem:(NSString * _Nonnull)uniqueId completion:(void (^ _Nonnull)(void * _Nullable (*)(void), NSInteger * _Nullable))completion {
-    <#code#>
+- (void)updateMemoItem:(NSString * _Nonnull)uniqueId
+            completion:(void (^ _Nonnull)(void * _Nullable (*)(void), NSInteger * _Nullable))completion {
+    [self readMemoItem:uniqueId completion:^(MemoItem * _Nullable memoItem, NSInteger * _Nullable errorCode) {
+        if (errorCode != nil) {
+            completion(nil, errorCode);
+        } else {
+            NSManagedObjectContext * _Nullable context = memoItem.managedObjectContext;
+            if (context == nil) {
+                completion(nil, NotFoundContext);
+            } else {
+                [context performBlockAndWait:^{
+                    // メモ内容更新
+
+                    [self.memoItemDataStore save:memoItem];
+                }];
+                completion(nil, nil);
+            }
+        }
+    }];
 }
 
 @end
